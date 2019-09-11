0306c1c4904e93075ef8293ce07c5db23b087fab
Sebastián Gurín <sgurin@awalabs.com>
2019-09-11 07:31:17 -0300

    warpPolar, estimateAffine2D tests

--- modules/js/test/test_calib3d.js
+++ modules/js/test/test_calib3d.js
@@ -41,3 +41,27 @@ QUnit.test('findHomography', function(assert) {
 
   assert.ok(mat instanceof cv.Mat);
 });
+
+QUnit.test('estimateAffine2D', function(assert) {
+   const inputs = cv.matFromArray(4, 1, cv.CV_32FC2, [
+    1, 1, 
+    80, 0, 
+    0, 80, 
+    80, 80
+  ]);
+  const outputs = cv.matFromArray(4, 1, cv.CV_32FC2, [
+    21, 51, 
+    70, 77, 
+    40, 40, 
+    10, 70
+  ]);
+  const M = cv.estimateAffine2D(inputs, outputs);
+  assert.ok(M instanceof cv.Mat);
+  assert.deepEqual(Array.from(M.data), [
+     23,  55,  97, 126,  87, 139, 227,  63,   0,   0,
+      0,   0,   0,   0, 232, 191,  71, 246,  12,  68,
+    165,  35,  53,  64,  99,  56,  27,  66,  14, 254,
+    212,  63, 103, 102, 102, 102, 102, 102, 182, 191,
+    195, 252, 174,  22,  55,  97,  73,  64
+  ]);  
+});
